a5da645d553f510739c44ddc975a64bd2d472032
Nevin Raju ( നെവിൻ ) <106428255+newwohh@users.noreply.github.com>
2024-11-11 16:03:06 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,6 +35,13 @@ const IndexPage = () => {
             })(window, document, "clarity", "script", "opscxmemc8");
           `}
         </script>
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KJJ76XHN');</script>
+<!-- End Google Tag Manager -->
       </Helmet>
 
       <div className={darkMode ? "dark" : ""}>
@@ -182,6 +189,11 @@ const IndexPage = () => {
           </footer>
         </div>
       </div>
+
+          <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KJJ76XHN"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     </>
   );
 };
